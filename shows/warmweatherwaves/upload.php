89b77d2b75f8fd5146a189871e84e3fc97ead12d
timothy kreider <timothykreider13@gmail.com>
2019-06-20 14:13:59 -0400

    fixed some css and hopefully the image upload

--- shows/warmweatherwaves/upload.php
+++ shows/warmweatherwaves/upload.php
@@ -47,13 +47,10 @@
     
 
   <?php
-        
-    $fileName = $_FILES["fileToUpload"]["tmp_name"];
-$target_dir = ("images/" . $fileName);
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -63,37 +60,6 @@ if(isset($_POST["submit"])) {
     } else {
         echo "File is not an image.";
         $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    echo "</br>";
-    $path = "uploads/" . $_FILES['fileToUpload']["name"];
-    echo $path;
-    
-    if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $path )) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
     }
 }
 ?>
